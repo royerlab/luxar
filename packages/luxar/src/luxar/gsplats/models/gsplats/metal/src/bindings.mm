7b630f5362d0b91698f35d3b4228e57a88d26a17
royer <royerloic@gmail.com>
2026-05-02 10:39:11 -0700

    experiment(metal): try 32-thread splat groups

--- packages/luxar/src/luxar/gsplats/models/gsplats/metal/src/bindings.mm
+++ packages/luxar/src/luxar/gsplats/models/gsplats/metal/src/bindings.mm
@@ -25,7 +25,7 @@ struct uint3 {
     uint32_t x, y, z;
 };
 
-constexpr uint32_t kThreadgroupSize = 64;
+constexpr uint32_t kThreadgroupSize = 32;
 
 // ============================================================================
 // Metal Context Management
