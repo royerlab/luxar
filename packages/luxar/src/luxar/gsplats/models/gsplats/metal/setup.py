5d9680d4e33c2be8cd5c0a458367a5dcf4af98dd
royer <royerloic@gmail.com>
2026-05-02 10:46:34 -0700

    Revert "experiment(metal): compile shaders with fast math"
    This reverts commit 04e9809f09864ab6a04dd5e0126518bb9af407c8.

--- packages/luxar/src/luxar/gsplats/models/gsplats/metal/setup.py
+++ packages/luxar/src/luxar/gsplats/models/gsplats/metal/setup.py
@@ -46,7 +46,6 @@ def compile_metal_shaders() -> None:
             str(air_file),
             "-std=metal3.0",  # Use Metal 3.0 for SIMD intrinsics
             "-O2",  # Optimization level
-            "-ffast-math",  # Match the kernels' fast math assumptions
         ]
     )
 
