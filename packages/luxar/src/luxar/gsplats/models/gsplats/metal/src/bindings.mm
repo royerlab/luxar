01980fe3474008c8538bbc1b6f0391f66f055a20
royer <royerloic@gmail.com>
2026-05-02 10:40:20 -0700

    experiment(metal): split forward and backward group sizes

--- packages/luxar/src/luxar/gsplats/models/gsplats/metal/src/bindings.mm
+++ packages/luxar/src/luxar/gsplats/models/gsplats/metal/src/bindings.mm
@@ -25,7 +25,8 @@ struct uint3 {
     uint32_t x, y, z;
 };
 
-constexpr uint32_t kThreadgroupSize = 64;
+constexpr uint32_t kForwardThreadgroupSize = 256;
+constexpr uint32_t kBackwardThreadgroupSize = 64;
 
 // ============================================================================
 // Metal Context Management
@@ -294,7 +295,7 @@ torch::Tensor compute_conic_metal(torch::Tensor Ls) {
     [enc setBytes:&n_splats length:sizeof(uint32_t) atIndex:2];
 
     MTLSize threads = MTLSizeMake(n_splats, 1, 1);
-    MTLSize group = MTLSizeMake(std::min<uint32_t>(kThreadgroupSize, n_splats), 1, 1);
+    MTLSize group = MTLSizeMake(std::min<uint32_t>(kForwardThreadgroupSize, n_splats), 1, 1);
     [enc dispatchThreads:threads threadsPerThreadgroup:group];
     [enc endEncoding];
     [cmd commit];
@@ -338,7 +339,7 @@ torch::Tensor dispatch_forward_splat_3d(
         setBufferWithOffset(enc, output, 0);
         [enc setBytes:&total_pixels length:sizeof(uint32_t) atIndex:1];
         MTLSize threads = MTLSizeMake(total_pixels, 1, 1);
-        MTLSize group = MTLSizeMake(std::min<uint32_t>(kThreadgroupSize, total_pixels), 1, 1);
+        MTLSize group = MTLSizeMake(std::min<uint32_t>(kForwardThreadgroupSize, total_pixels), 1, 1);
         [enc dispatchThreads:threads threadsPerThreadgroup:group];
         [enc endEncoding];
     }
@@ -369,7 +370,7 @@ torch::Tensor dispatch_forward_splat_3d(
         [enc setBytes:&inv_one_minus_C length:sizeof(float) atIndex:9];
 
         MTLSize groups = MTLSizeMake(n_splats, 1, 1);
-        MTLSize threadsPerGroup = MTLSizeMake(kThreadgroupSize, 1, 1);
+        MTLSize threadsPerGroup = MTLSizeMake(kForwardThreadgroupSize, 1, 1);
         [enc dispatchThreadgroups:groups threadsPerThreadgroup:threadsPerGroup];
         [enc endEncoding];
     }
@@ -449,7 +450,7 @@ std::vector<torch::Tensor> dispatch_backward_splat_3d(
     [enc setBytes:&inv_one_minus_C length:sizeof(float) atIndex:12];
 
     MTLSize groups = MTLSizeMake(n_splats, 1, 1);
-    MTLSize threadsPerGroup = MTLSizeMake(kThreadgroupSize, 1, 1);
+    MTLSize threadsPerGroup = MTLSizeMake(kBackwardThreadgroupSize, 1, 1);
     [enc dispatchThreadgroups:groups threadsPerThreadgroup:threadsPerGroup];
     [enc endEncoding];
     [cmd commit];
