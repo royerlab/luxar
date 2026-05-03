7ff876fa532fce96af1525519185cb6ddc550a25
royer <royerloic@gmail.com>
2026-05-03 06:03:47 -0700

    Revert "experiment(metal): retest 128-thread raw splat groups"
    This reverts commit 00b76bc575faa1d6f0f944c21eac5e7bc2bceea8.

--- packages/luxar/src/luxar/gsplats/models/gsplats/metal/src/bindings.mm
+++ packages/luxar/src/luxar/gsplats/models/gsplats/metal/src/bindings.mm
@@ -25,7 +25,7 @@ struct uint3 {
     uint32_t x, y, z;
 };
 
-constexpr uint32_t kThreadgroupSize = 128;
+constexpr uint32_t kThreadgroupSize = 64;
 
 // ============================================================================
 // Metal Context Management
