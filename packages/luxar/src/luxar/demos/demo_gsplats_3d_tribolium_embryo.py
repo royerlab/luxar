0ccc854b87829f6a37be6797e9d36defb86e6210
Loic A. Royer <royerloic@gmail.com>
2026-08-25 02:09:15 -0700

    Refactor download helpers and remote ZIP reader (#2107)
    * refactor(demos): barrel download helpers (#2076)
    
    * refactor(utils): split remote zip reader (#2076)
    
    * style(utils): normalize moved imports (#2076)
    
    * docs(demos): align split helper guidance (#2076)
    
    * docs(utils): document remote zip move (#2076)
    
    * docs(demos): clarify download helper exports (#2076)
    
    ---------
    
    Co-authored-by: Loic Royer <loic.royer@czbiohub.org>

--- packages/luxar/src/luxar/demos/demo_gsplats_3d_tribolium_embryo.py
+++ packages/luxar/src/luxar/demos/demo_gsplats_3d_tribolium_embryo.py
@@ -218,7 +218,7 @@ def download_tribolium_data() -> Path:
     Returns:
         Path to downloaded ZIP file.
     """
-    from luxar.utils.download import robust_download
+    from luxar.demos import robust_download
 
     zip_path = CACHE_DIR / "Supplemental_File_2.zip"
 
