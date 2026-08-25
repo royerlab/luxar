06ab3f6bc72d923968801b51e6059542bfa52e6c
Loic Royer <loic.royer@czbiohub.org>
2026-08-24 18:37:18 -0700

    test(coverage): reject extra utility omissions

--- packages/luxar/src/luxar/demos/tests/test_demo_import_spelling.py
+++ packages/luxar/src/luxar/demos/tests/test_demo_import_spelling.py
@@ -284,6 +284,7 @@ def test_coverage_omits_only_the_split_demo_utilities() -> None:
     }
     split_patterns = {f"*/luxar/utils/{module}" for module in split_modules}
     assert split_patterns <= set(omit)
+    assert {pattern for pattern in omit if "/utils/" in pattern} == split_patterns
 
     matcher = GlobMatcher(omit)
     for module in split_modules:
